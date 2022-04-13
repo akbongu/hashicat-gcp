1e47cf14b0b2884e5fa03fd5acae38c52e00628a
Arun Bongu <akbongu@gmail.com>
2022-04-13 18:51:22 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -6,7 +6,7 @@
 # prompted to enter it (or define it via config file or command line flags.)
 
 variable "project" {
-  description = "ID of your GCP project. Make sure you set this up before running this terraform code.  REQUIRED."
+  description = "p-gon78wtu37o18jscfxo7fql5tcfh"
 }
 
 variable "prefix" {
