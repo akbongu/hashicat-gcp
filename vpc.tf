ae268df3615e14da7cd7fb2e8f16ce4838c3c229
Arun Bongu <akbongu@gmail.com>
2022-04-13 20:14:28 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,15 @@
-subnets = [
+module "network" {
+  source  = "app.terraform.io/arunbongu-training/network/google"
+  version = "3.4.0"
+  subnets = [
   {
     subnet_name   = "akbongu-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+}
+   
+
+
+
